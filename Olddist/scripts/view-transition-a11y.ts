7f8b6632896e3cf432c492f12e876238ae91ac3f
James Holloway <studio@dovetaildigital.co.uk>
2025-06-27 16:59:22 +0100

    Removal of old dist

--- Olddist/scripts/view-transition-a11y.ts
+++
@@ -1,9 +0,0 @@
-document.addEventListener('astro:after-swap', () => {
-    const main = document.querySelector('main');
-    if (main) {
-      const heading = main.querySelector('h1, h2, [role="heading"]');
-      if (heading && heading instanceof HTMLElement) heading.focus();
-      else main.setAttribute('tabindex', '-1'), main.focus();
-    }
-  });
-  
